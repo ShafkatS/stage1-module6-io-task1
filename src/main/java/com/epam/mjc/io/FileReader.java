8da918bfb5feecee1b526cd4e0c8a4c6fd268d76
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-19 13:59:10 +0500

    Update FileReader.java

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -6,6 +6,17 @@ import java.io.File;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder str = new StringBuilder();
+        try (FileInputStream fileInputStream = new FileInputStream(file.getAbsoluteFile());) {
+            int c;
+            while ((c = fileInputStream.read()) != -1) {
+                char c1 = (char) c;
+                str.append(c1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile("Anna", 25, "anna@mailserver.com", 1234567890L);
+    }
     }
 }
